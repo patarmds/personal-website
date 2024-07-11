e3a9f8bba53ef6a21a0f56f4944de46cc971ade4
Patar Martua Doli Siahaan <patarismyname@gmail.com>
2024-07-11 15:21:37 +0700

    add email

--- src/components/header.js
+++ src/components/header.js
@@ -5,23 +5,12 @@ export default function Header({pageName}){
       return <>
        <li><Link href="/" className={`${pageName == "about" ? "active bg-gradient-to-r from-pink-500 to-yellow-500 font-bold" : ""}`}><span className="text-white">About</span></Link></li>
         <li><Link href="/project" className={`${pageName == "project" ? "active bg-gradient-to-r from-pink-500 to-yellow-500 font-bold" : ""}`}><span className="text-white">Project</span></Link></li>
-        <li><Link href="/reachme" className={`${pageName == "contact" ? "active bg-gradient-to-r from-pink-500 to-yellow-500 font-bold" : ""}`}><span className="text-white">Contact</span></Link></li>
+        {/* <li><Link href="/reachme" className={`${pageName == "contact" ? "active bg-gradient-to-r from-pink-500 to-yellow-500 font-bold" : ""}`}><span className="text-white">Contact</span></Link></li> */}
       </>
     }
 
     return (
     <header className="">
-       
-    {/* <Image src={"/web_logo.png"} className="m-auto pb-2" width={64} height={64} alt="logo"/> */}
-     
-     {/* <h1 className="text-2xl text-center font-bold italic">Patar.dev </h1>  */}
-     {/* <h1 class="animate-text bg-gradient-to-r from-blue-500 via-sky-300 to-white bg-clip-text text-transparent text-2xl text-center font-bold italic">Patar.dev</h1> */}
-     {/* <div className="w-3/4 sm:w-2/3 md:w-1/2 mx-auto m-2 pt-0.5 bg-gradient-to-r from-blue-500 to-sky-500 h-1"></div>
-     <div className="flex justify-center items-center space-x-1 navbar">
-       <Link href="/" className={`py-4 px-2 text-white-500 menu ${pageName == "about" ? "active" : ""}`}>About</Link>
-       <Link href="/project" className={`py-4 px-2 text-white-500 menu ${pageName == "project" ? "active" : ""}`}>Project</Link>
-       <Link href="/reachme" className={`py-4 px-2 text-white-500 menu ${pageName == "reachme" ? "active" : ""}`}>Reach Me!</Link>
-     </div> */}
      <div className="navbar w-100 md:w-2/3 m-auto border-b border-b-4 ">
   <div className="navbar-start">
     <div className="dropdown">
--- src/pages/index.js
+++ src/pages/index.js
@@ -30,9 +30,13 @@ export default function About() {
                             <h2 className="text-5xl 3xl:text-6xl font-bold">You can call me, <span className="bg-gradient-to-r from-pink-500 to-yellow-500 inline-block text-transparent bg-clip-text">Patar</span></h2>
                         </div>
                         <div className="mb-4">
-                            <a className="btn bg-sky-500 text-white font-bold rounded-full bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500" href="https://linkedin.com/in/patarshn" target="_blank">
-                                <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
+                            <a className="m-1 btn bg-sky-500 text-white font-bold rounded-full bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500" href="https://linkedin.com/in/patarshn" target="_blank">
+                                <svg className="fill-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></svg>
                                 LinkedIn
+                            </a>
+                            <a className="m-1 btn bg-sky-500 text-white font-bold rounded-full bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500" href="https://linkedin.com/in/patarshn" target="_blank">
+                            <svg className="fill-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm21.518 2l-9.518 7.713-9.518-7.713h19.036zm-19.518 14v-11.817l10 8.104 10-8.104v11.817h-20z"/></svg>
+                                Email
                             </a>
                         </div>
                         <div className="border-l-4 border-t rounded-tl-lg p-4">
